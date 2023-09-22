a8d6508cae33ded2c5747f06c781741a4afb8ab4
Akash Dasgupta <bloombergakash@gmail.com>
2023-09-22 17:17:19 +0530

    Update: social changes

--- src/components/home/social.tsx
+++ src/components/home/social.tsx
@@ -14,7 +14,7 @@ import {
   Twitter,
   YouTube,
 } from "@mui/icons-material";
-import { Box, Stack, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Stack, SxProps, useMediaQuery, useTheme } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import React from "react";
 import fb from "./images/fb.svg";
@@ -56,7 +56,7 @@ const Social = () => {
         rowGap={{ xs: 2, sm_md: 0 }}
         columnSpacing={{ xs: 2, sm_md: 0 }}
       >
-        <Grid2 xs={6} sm_md={4}>
+        <Grid2 xs={6} sm_md={4} sx={gridHover}>
           <SocialCard
             img={
               "https://s3-alpha-sig.figma.com/img/fc1b/dfad/c4c32a16e73b950d463e2853f59a3c99?Expires=1696204800&Signature=D8MXXMWuSFR9OCqGAj1QCFA0UZVdYFV3HhNSPMEky9FhXNz5B6pzJGHbFlPHR0~U-~~FL2Oq8rck2CkGpsXBwTR0kqODKbiX2V0Sxcg37HDX2iGHiid6VkFKvG4k6L5ecOrgpZQWM9jvSnifCF2vx75GpddF2Dj6nFktZAHbHpmEg0QyiMM2uYgECT1RO1VMKqqHw4qgkqXKsYbzVcGOiqyT0he92U6qATaeZitMV-O4UQwXBeBz8uAosTi6n6B5bSutyf6aLeTQtlNh7Y~4snMgAW1Tm0woteIFUVOANIioooOfCdRUGQ-E~KmRuf0FvUpR~Qt6NOBrVj3pVMwCbQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
@@ -75,6 +75,7 @@ const Social = () => {
             display={"flex"}
             justifyContent={"center"}
             alignItems={"center"}
+            sx={gridHover}
           >
             <MotionTypography
               variant='h1'
@@ -87,7 +88,7 @@ const Social = () => {
           </Grid2>
         )}
         {!matches && (
-          <Grid2 xs={4} overflow='hidden'>
+          <Grid2 xs={4} sx={gridHover} overflow='hidden'>
             <SocialCard
               img={
                 "https://s3-alpha-sig.figma.com/img/91a1/a459/2d9ef5167cdce4c6779f48b8cd85fde4?Expires=1696204800&Signature=eH9Gawh3waJ5Yl5NdZ1~twC6UgNXBolGR482h1d1uidvc7j~bJk6TBTK8wPJW5bcVQn4iC1rtgl7pgZAyC~v4NuIY1H5SgYsTxsXuMqPUSHUYf5IBmvmL3W-2qmjzqMzcj9IXINOXQuN~KW7JfBHhrdv3Ou2EUVw6KwzC9daO6QfLE9qdpboSQWmwsDQ23Li9vt6XYtLsKuMG8YwkLeCbd3-vaiExEf2CgQYLeCvfEsVZLdRjumVfGo5S-1ZkMeOm7ngVy7MdAw9HHUOxOT~CiBYcFtCUGo1-nvO6G2vu9a1YsqqPQA157kE1vEz4gjUBZniLRV1sdnVDkmt4MUZbg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
@@ -97,7 +98,7 @@ const Social = () => {
             />
           </Grid2>
         )}
-        <Grid2 xs={6} sm_md={4} overflow='hidden'>
+        <Grid2 xs={6} sm_md={4} sx={gridHover} overflow='hidden'>
           <SocialCard
             img={
               "https://s3-alpha-sig.figma.com/img/7775/10fe/6f54cc5ebb1bb1075ce3fe6b9245e704?Expires=1696204800&Signature=f2ZClAxBwyxAgJIBu2dP6w~mfzAH3U46Xb6moEIlQKfl4F~GH3ONAg3TdzBFha~i99VqIUMkYfPiezLERLGj24f14vIGQ3UDFWrsDOBcsRk0tRpr7ZL4Syk~ZbH6ND94OazhwkrlyTyW8vilRyx-qBxkZVqjGzt0UDdsl9F~CbS5mGzu05FMN-bPPROeupaD66fdtSb2eQkTAFLmAnESNNbbMgTj2HS3RXTCmJQWT8fl5CZ10fIFM-QbzDdtUxb~tMypfEqKOvUoLW9p0Koupa70zKM~DHNY3mql8aBmAuEb158j1yVcwZa3Fc8j1CnZZ2o~YmEDjyXAHqVlQppcyg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
@@ -106,7 +107,7 @@ const Social = () => {
             {...cardTransitionRight(1)}
           />
         </Grid2>
-        <Grid2 xs={6} sm_md={4} overflow='hidden'>
+        <Grid2 xs={6} sm_md={4} sx={gridHover} overflow='hidden'>
           <SocialCard
             img={
               "https://s3-alpha-sig.figma.com/img/02cb/58c2/aeaa50aeb369b72d106951eb60aa2799?Expires=1696204800&Signature=fYyAz3RkrMFQJWyM5NHVRSKaJoJCfOUNgJiOrunEqwrN23LtPtK5ls4xY14mcGiV49wDoLpL3FjN8jDpf7plNlZfvTIexj0HMR5wuT4irkkC4PNWTewLriw~nTh56AbgzOa2CbKmkSj6eWo51rvA~YhpFWzAHGVlvkCFb9c6rAM5kvhh9yr8skfPF-q5EPSJl-a8FJa7pq1T1VMAogZkXNtDyfrSgQtO5ANudbSUKRRcX-0xg7vbyStrM86GCOk17gUeYSDCQbS3YSGwH6f-r4L9Fe77cMc4WyUF1j~UaXroB55WJURTDVnLwbJY8QQhIt4vHuuf0omFu-77cQR5qg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
@@ -146,7 +147,7 @@ const Social = () => {
             </Stack>
           </Grid2>
         )}
-        <Grid2 xs={6} sm_md={4} overflow='hidden'>
+        <Grid2 xs={6} sm_md={4} sx={gridHover} overflow='hidden'>
           <SocialCard
             img={
               "https://s3-alpha-sig.figma.com/img/2f89/8169/64d012ae3609a395c2a555f5595cea8c?Expires=1696204800&Signature=cvCmCrcx17~2RocD7bj7uPPstibXX3SzmaZj5ffcYAao1N3EXl9ozxqxhOxCJ-vaWaq5zpD~W7OwfOL57max71ZLgGPcZdhw3Z8CKNxmttkbWuSRrEyEezIbhZbYKN2Sgq5BQSwCWeIe~I1t8vOJ5GBNBHZRlmG-DSDZvIWM-7FwQVb9mbPAUC-gyaAD4FhwVSWT~e6zoajoBeLCz1ibGqClN2KZQR-2KpTOTDTki7b-qoQpFZden1OF7b0HyulL-do9PU5XKtqONxAZF9wNBFDlcVDy~Y~3AiGLwYwdCX0W-mMjlGvZNBL9ocfJria9ThyqEdf8AdUC0kNztfbTYg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
@@ -185,7 +186,15 @@ const Social = () => {
 
 export default Social;
 
-const cardTransitionDown = (delay: number) => {
+const gridHover: SxProps = {
+  "&:hover": {
+    transform: "scale(1.1)",
+    transition: "all 0.3s",
+    zIndex: 1,
+  },
+};
+
+const cardTransitionDown = (delay: number): MotionCardProps => {
   return {
     initial: {
       y: "-100%",
@@ -199,9 +208,12 @@ const cardTransitionDown = (delay: number) => {
         duration: 0.4,
       },
     },
-  } as MotionCardProps;
+    viewport: {
+      once: true,
+    },
+  };
 };
-const cardTransitionRight = (delay: number) => {
+const cardTransitionRight = (delay: number): MotionCardProps => {
   return {
     initial: {
       x: "-100%",
@@ -215,5 +227,8 @@ const cardTransitionRight = (delay: number) => {
         duration: 0.4,
       },
     },
-  } as MotionCardProps;
+    viewport: {
+      once: true,
+    },
+  };
 };
