1c98cfedb7d0d757c7206f251eae8365b7b75e5b
Akash Dasgupta <bloombergakash@gmail.com>
2023-09-22 18:33:55 +0530

    Update: Cards wrapped with Link

--- src/commonComponents/cards/smallTitleCard.tsx
+++ src/commonComponents/cards/smallTitleCard.tsx
@@ -2,10 +2,12 @@ import { CardContent, Typography } from "@mui/material";
 import React, { forwardRef } from "react";
 import { EnhancedSwiperSlideData } from "@cc/enhanced-swiper";
 import { MotionCard, MotionCardProps } from "@cc/motion-elements";
+import Link from "next/link";
 
 interface SmallTitleCardProps extends Omit<MotionCardProps, "children"> {
   img: string;
   title: string;
+  href: string;
 }
 
 export const SmallTitleCard: EnhancedSwiperSlideData<SmallTitleCardProps> =
@@ -13,6 +15,7 @@ export const SmallTitleCard: EnhancedSwiperSlideData<SmallTitleCardProps> =
     const {
       img,
       title,
+      href,
       sx,
       index,
       isPrev,
@@ -22,32 +25,34 @@ export const SmallTitleCard: EnhancedSwiperSlideData<SmallTitleCardProps> =
       ...restProps
     } = props;
     return (
-      <MotionCard
-        ref={ref}
-        sx={{
-          display: "flex",
-          position: "relative",
-          width: { xs: 200, lg: 328 },
-          height: { xs: 200, lg: 316 },
-          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.50) 0%, rgba(0, 0, 0, 0.00) 34.18%), url(${img}), lightgray -133.577px -1.973px / 181.45% 101.249% no-repeat`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          borderRadius: 0,
-          ...sx,
-        }}
-        {...restProps}
-      >
-        <CardContent
+      <Link href={href}>
+        <MotionCard
+          ref={ref}
           sx={{
-            alignSelf: "flex-end",
-            pb: "16px !important",
+            display: "flex",
+            position: "relative",
+            width: { xs: 200, lg: 328 },
+            height: { xs: 200, lg: 316 },
+            background: `linear-gradient(0deg, rgba(0, 0, 0, 0.50) 0%, rgba(0, 0, 0, 0.00) 34.18%), url(${img}), lightgray -133.577px -1.973px / 181.45% 101.249% no-repeat`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            borderRadius: 0,
+            ...sx,
           }}
+          {...restProps}
         >
-          <Typography variant='h5' fontWeight={400} color={"white"}>
-            {title}
-          </Typography>
-        </CardContent>
-      </MotionCard>
+          <CardContent
+            sx={{
+              alignSelf: "flex-end",
+              pb: "16px !important",
+            }}
+          >
+            <Typography variant='h5' fontWeight={400} color={"white"}>
+              {title}
+            </Typography>
+          </CardContent>
+        </MotionCard>
+      </Link>
     );
   });
 
--- src/commonComponents/cards/social.tsx
+++ src/commonComponents/cards/social.tsx
@@ -1,38 +1,42 @@
 import { SvgIconComponent } from "@mui/icons-material";
 import { MotionCard, MotionCardProps } from "../motion-elements";
 import { CardContent } from "@mui/material";
+import Link from "next/link";
 
 interface SocialCardProps extends Omit<MotionCardProps, "children"> {
   img: string;
+  href: string;
   Icon: SvgIconComponent;
 }
 const SocialCard = (props: SocialCardProps) => {
-  const { img, Icon, sx, ...restProps } = props;
+  const { img, href, Icon, sx, ...restProps } = props;
   return (
-    <MotionCard
-      sx={{
-        display: "flex",
-        position: "relative",
-        width: { xs: 1, lg: 289 },
-        aspectRatio: 1,
-        background: `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.67) 100%), url(${img})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        borderRadius: 0,
-        ...sx,
-      }}
-      {...restProps}
-    >
-      <CardContent
+    <Link href={href}>
+      <MotionCard
         sx={{
-          alignSelf: "flex-end",
-          p: { xs: 1, sm_md: 2.5 },
-          pb: { xs: "8px !important", sm_md: "16px !important" },
+          display: "flex",
+          position: "relative",
+          width: { xs: 1, lg: 289 },
+          aspectRatio: 1,
+          background: `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.67) 100%), url(${img})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          borderRadius: 0,
+          ...sx,
         }}
+        {...restProps}
       >
-        <Icon sx={{ color: "white", fontSize: { xs: 16, sm_md: 26 } }} />
-      </CardContent>
-    </MotionCard>
+        <CardContent
+          sx={{
+            alignSelf: "flex-end",
+            p: { xs: 1, sm_md: 2.5 },
+            pb: { xs: "8px !important", sm_md: "16px !important" },
+          }}
+        >
+          <Icon sx={{ color: "white", fontSize: { xs: 16, sm_md: 26 } }} />
+        </CardContent>
+      </MotionCard>
+    </Link>
   );
 };
 export default SocialCard;
