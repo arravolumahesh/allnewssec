a1f943f4285d4c1877ff50447f5faba941b716a0
SURAJ GUPTA <37866971+srgsid@users.noreply.github.com>
2023-09-23 23:26:29 +0530

    Remove unnecessary theme styling in Material components
    Removed theme unstable_sx styling from MaterialImage, MaterialSwiper, and MaterialSwiperSlide components in the material-components file. This styling was redundant as the components did not require it. Simplifying the code improves readability and performance.

--- src/commonComponents/material-components.ts
+++ src/commonComponents/material-components.ts
@@ -3,16 +3,9 @@ import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { ComponentProps } from "react";
 
-export const MaterialImage = styled(Image)(({ theme }) => {
-  return theme.unstable_sx({});
-});
-
-export const MaterialSwiper = styled(Swiper)(({ theme }) => {
-  return theme.unstable_sx({});
-});
-export const MaterialSwiperSlide = styled(SwiperSlide)(({ theme }) => {
-  return theme.unstable_sx({});
-});
+export const MaterialImage = styled(Image)();
+export const MaterialSwiper = styled(Swiper)();
+export const MaterialSwiperSlide = styled(SwiperSlide)();
 
 export type MaterialImageProps = ComponentProps<typeof MaterialImage>;
 export type MaterialSwiperProps = ComponentProps<typeof MaterialSwiper>;
