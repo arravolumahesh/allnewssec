6f58b1270c74c3d28eae7d27bdc5c78725b7b833
SURAJ GUPTA <37866971+srgsid@users.noreply.github.com>
2023-09-26 20:40:04 +0530

    Improve button styling and add motion to footer component
    This commit modifies the styling rules for various button types in animated-button.tsx to ensure that none of them have a border, even on hover. This change simplifies the look of the buttons and ensures a more consistent appearance across different types. In addition, the footer component is updated in index.tsx to include motion properties, enhancing the visual experience for users by introducing a subtle loading animation.

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -4,6 +4,8 @@ import SocialLinks from "@/layout/footer/social-links";
 import CommonLinks from "@/layout/footer/common-links";
 import Copyright from "@/layout/footer/copyright";
 import { alpha, Divider, SxProps, Theme } from "@mui/material";
+import { MotionVariantProps } from "@cc/motion-components";
+import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
@@ -14,8 +16,11 @@ const Footer = () => {
           background: (theme) => theme.palette.gradient.lightToDark,
         },
       }}
-      rowGap={{ xs: 0, sm: 7, lg: 9 }}
+      rowGap={{ xs: 0, sm: 2, lg: 9 }}
       divider={<Divider sx={dividerSx} />}
+      component={motion.div}
+      initial={"initial"}
+      whileInView={"animate"}
     >
       <SocialLinks />
       <CommonLinks />
@@ -33,4 +38,18 @@ const dividerSx: SxProps<Theme> = (theme) => {
     mt: "32px !important",
     mb: "20px !important",
   };
+};
+
+const footerChildrenMotionVariants: MotionVariantProps = {
+  initial: {
+    opacity: 0,
+    x: "40%",
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
 };
