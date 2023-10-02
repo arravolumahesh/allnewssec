f05ffc75f18c1fd9abbb167aee92e1f037cfff01
Akash Dasgupta <bloombergakash@gmail.com>
2023-10-02 14:24:03 +0530

    Update: Section navigation

--- src/app/our-companies/page.tsx
+++ src/app/our-companies/page.tsx
@@ -16,7 +16,12 @@ const Company = () => {
   return (
     <>
       <BuildFuture />
-      <SectionNavigation menus={menus} />
+      <SectionNavigation
+        logoImg={
+          "https://s3-alpha-sig.figma.com/img/db64/8e41/5817c9640f87a5e65c72730fd3c68df2?Expires=1696809600&Signature=CxQFzRXfbXHrEnJKXnG95laUmKKq0nOqnAW3CoeP6OJE8EgqbwkdNVdkbz8K8ya3yuv0M9gwESb1P9Afo80u4LFknhiblu3NbFUdVnwE37E2DRmDAxGQfxVuF0YuWaph~XlnMjaiXRowaPo-~TnQaSojfiLGvazwMENkDsq55s3KFItCsjDwnzuOvSoOQzGlljE9Ef1qBtcSa2Sn3RchKZqkN07-weEozkD7KHgdieV1CyHeCSq1c5hT4ZziO0JK8fTKmGtBrPbGvkkNbb7LhWBtkLrhbhZT4PyV69qAGfMVvaA8eKliQFEI53JIyKicQBE0gsmG6Nw1XZ~DByHKbg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+        }
+        menus={menus}
+      />
       <Happening />
       <OurFocusAreas />
       <Stories />
--- src/commonComponents/section-navigation.tsx
+++ src/commonComponents/section-navigation.tsx
@@ -60,22 +60,22 @@ const SectionNavigation = (props: SectionMenuProps) => {
         color={"primary.main"}
         {...WrapperProps}
       >
-        <MaterialImage
-          src={
-            "https://s3-alpha-sig.figma.com/img/db64/8e41/5817c9640f87a5e65c72730fd3c68df2?Expires=1696809600&Signature=CxQFzRXfbXHrEnJKXnG95laUmKKq0nOqnAW3CoeP6OJE8EgqbwkdNVdkbz8K8ya3yuv0M9gwESb1P9Afo80u4LFknhiblu3NbFUdVnwE37E2DRmDAxGQfxVuF0YuWaph~XlnMjaiXRowaPo-~TnQaSojfiLGvazwMENkDsq55s3KFItCsjDwnzuOvSoOQzGlljE9Ef1qBtcSa2Sn3RchKZqkN07-weEozkD7KHgdieV1CyHeCSq1c5hT4ZziO0JK8fTKmGtBrPbGvkkNbb7LhWBtkLrhbhZT4PyV69qAGfMVvaA8eKliQFEI53JIyKicQBE0gsmG6Nw1XZ~DByHKbg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-          }
-          width={124}
-          height={32}
-          sx={{
-            width: { xs: 94, md: 124 },
-            height: { xs: 24, md: 32 },
-          }}
-          alt="Company Name"
-        />
+        {logoImg && (
+          <MaterialImage
+            src={logoImg}
+            width={124}
+            height={32}
+            sx={{
+              width: { xs: 94, md: 124 },
+              height: { xs: 24, md: 32 },
+            }}
+            alt='Company Name'
+          />
+        )}
         <Tabs
           value={value}
           onChange={handleChange}
-          variant="scrollable"
+          variant='scrollable'
           scrollButtons={false}
           sx={{
             width: 1,
