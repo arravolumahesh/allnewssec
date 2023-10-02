b2a4321e6ad000b590a0af96ef26c40c701cb30f
SURAJ GUPTA <37866971+srgsid@users.noreply.github.com>
2023-10-02 15:32:42 +0530

    Comment out imports and elements in CaseStudy component
    Temporarily commented

--- src/app/case-stady/page.tsx
+++ src/app/case-stady/page.tsx
@@ -1,21 +1,21 @@
-import ChallengesFaced from "@/components/caseStudy/challenges-faced";
-import GallaryWall from "@/components/caseStudy/gallary-wall";
-import CaseStudies from "@/components/caseStudy/more-casee-studies";
-import CaseHome from "@/components/caseStudy/IPH-Triveni-Program";
-import Counts from "@/components/caseStudy/counts";
-import Accomplished from "@/components/caseStudy/what-we-accomplished";
-import HowWeDid from "@/components/caseStudy/how-we-did-it";
+// import ChallengesFaced from "@/components/caseStudy/challenges-faced";
+// import GallaryWall from "@/components/caseStudy/gallary-wall";
+// import CaseStudies from "@/components/caseStudy/more-casee-studies";
+// import CaseHome from "@/components/caseStudy/IPH-Triveni-Program";
+// import Counts from "@/components/caseStudy/counts";
+// import Accomplished from "@/components/caseStudy/what-we-accomplished";
+// import HowWeDid from "@/components/caseStudy/how-we-did-it";
 
 const CaseStudy = () => {
   return (
     <>
-      <CaseHome />
-      <Counts />
-      <Accomplished />
-      <HowWeDid />
-      <ChallengesFaced />
-      <GallaryWall />
-      <CaseStudies />
+      {/*<CaseHome />*/}
+      {/*<Counts />*/}
+      {/*<Accomplished />*/}
+      {/*<HowWeDid />*/}
+      {/*<ChallengesFaced />*/}
+      {/*<GallaryWall />*/}
+      {/*<CaseStudies />*/}
     </>
   );
 };
