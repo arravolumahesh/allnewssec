f3a4ff3ac460eeef5247d73e9e810eafbac537c2
Akash Dasgupta <bloombergakash@gmail.com>
2023-10-04 19:39:20 +0530

    Add: Changes

--- src/components/ourCompanies/stories.tsx
+++ src/components/ourCompanies/stories.tsx
@@ -50,6 +50,8 @@ const data: ScrollTriggeredSwiperSectionProps["data"] = [
   {
     bgImage:
       "https://s3-alpha-sig.figma.com/img/1c35/79d3/44b7670550a3293788be4cd400109c86?Expires=1697414400&Signature=WnAa~Rex9cH~eyqYmoHl-rrs115m6ps~sEjxgmJMRhgMi7y3x9VFHNXASMclAjrjefKGITYTiUHNdf5Tt1jfGGdI7u01NWDWoa2Gn57rInAFJf6w2daPxxm3w7QEw1VO1dtQgYlinzdSj3SN8J7EQpkyGZRaUS2Uegb0y03mXI5G1PBvjODq1EzcY1jdSJnC2TkmUm-o1Xv69xmqEn6Bq4EIeDcSE2Y-s3zSJ5~POj0VikIjhN65Qxq3b8TsT316904lEw~acpMFdB6i58FVMvl51sKeUZb-PXQ3~xFfONMx1ijkDVMG4FM9YHH01tDEgiMQISv2p89Lqhi2Byj1Uw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    company: "Building a Skilled-Workforce",
+    title: "Creating Livelihoods Through Skilling",
     description: `Sunita, a resident of Mumbai’s slums, once struggled to make ends meet. With the Project Hamaara Sapna, her life took a hopeful turn. The initiative not only taught her sewing and tailoring but instilled confidence, breaking the shackles of underprivilege. More than a job, it’s a renewed life, a journey from marginalization to dignity, reflecting the essence of our foundation.`,
   },
 ];
