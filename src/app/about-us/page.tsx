8ec1eaa7568f0ebac65260ef199a7ff6f26dea5d
Akash Dasgupta <bloombergakash@gmail.com>
2023-10-09 16:19:11 +0530

    Add: Arrow section

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,3 +1,4 @@
+import ObjectiveSection from "@/commonComponents/objective-section";
 import AboutUsBanner from "@/components/aboutUs/banner";
 import OurPurpose from "@/components/aboutUs/our-purpose";
 import React from "react";
@@ -7,6 +8,21 @@ const AboutUs = () => {
     <>
       <AboutUsBanner />
       <OurPurpose />
+      <ObjectiveSection
+        Objective='Success is measured by the smiles we bring, the opportunities we create, and the positive change we ignite.'
+        Author='Jamnalal Bajaj'
+        Company='Founder, Bajaj Group'
+        SectionProps={{
+          sx: {
+            background: "background.default",
+          },
+        }}
+        color={"primary.main"}
+        SVGProps={{
+          width: 111,
+          height: 326,
+        }}
+      />
     </>
   );
 };
